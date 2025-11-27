6dc750acf53d3caccb10dddbe593ec0954cdebe2
Olivier Labayle <olabayle@gmail.com>
2025-11-27 11:05:48 +0000

    add saige binary gwas harmonization

--- src/harmonize_gwas_results.jl
+++ src/harmonize_gwas_results.jl
@@ -1,22 +1,53 @@
 function harmonize_gwas_results(gwas_results_file; source_software="saige", output="harmonized_results.tsv")
     gwas_results = CSV.read(gwas_results_file, DataFrame; missingstring="NA")
     harmonized_results = if source_software == "saige"
-        DataFrames.select(gwas_results,
-            :CHR => :CHROM,
-            :POS,
-            :MarkerID => :ID,
-            :Allele1 => :ALLELE_0,
-            :Allele2 => :ALLELE_1,
-            :AF_Allele2  => :ALLELE_1_FREQ,
-            :BETA,
-            :SE,
-            Symbol("p.value") => (x -> -log10.(x)) => :LOG10P,
-            :N,
-            :AC_Allele2 => :ALLELE_1_COUNT,
-            :MissingRate => :MISSING_RATE,
-            :Tstat => :T_STAT,
-            :var => :VAR,
-        )
+        # Binary trait
+        if "N_case" in names(gwas_results)
+            DataFrames.select(gwas_results,
+                :CHR => :CHROM,
+                :POS,
+                :MarkerID => :ID,
+                :Allele1 => :ALLELE_0,
+                :Allele2 => :ALLELE_1,
+                :AF_Allele2 => :ALLELE_1_FREQ,
+                :BETA,
+                :SE,
+                Symbol("p.value") => (x -> -log10.(x)) => :LOG10P,
+                [:N_case, :N_ctrl] => ((nca, nco) -> nca .+ nco) => :N,
+                :AC_Allele2 => :ALLELE_1_COUNT,
+                :MissingRate => :MISSING_RATE,
+                :Tstat => :T_STAT,
+                :var => :VAR,
+                :N_case => :N_CASES,
+                :N_ctrl => :N_CONTROLS,
+                Symbol("p.value.NA") => :PVAL_NA,
+                Symbol("Is.SPA") => :IS_SPA,
+                :AF_case => :AF_CASES,
+                :AF_ctrl => :AF_CONTROLS,
+                :N_case_hom => :N_CASES_HOM,
+                :N_case_het => :N_CASES_HET,
+                :N_ctrl_hom => :N_CONTROLS_HOM,
+                :N_ctrl_het => :N_CONTROLS_HET
+            )
+        # Continuous trait
+        else
+            DataFrames.select(gwas_results,
+                :CHR => :CHROM,
+                :POS,
+                :MarkerID => :ID,
+                :Allele1 => :ALLELE_0,
+                :Allele2 => :ALLELE_1,
+                :AF_Allele2  => :ALLELE_1_FREQ,
+                :BETA,
+                :SE,
+                Symbol("p.value") => (x -> -log10.(x)) => :LOG10P,
+                :N,
+                :AC_Allele2 => :ALLELE_1_COUNT,
+                :MissingRate => :MISSING_RATE,
+                :Tstat => :T_STAT,
+                :var => :VAR,
+            )
+        end
     elseif source_software == "regenie"
         DataFrames.select(gwas_results,
             :CHROM,
--- test/harmonize_gwas_results.jl
+++ test/harmonize_gwas_results.jl
@@ -12,6 +12,8 @@ TESTDIR = joinpath(PKGDIR, "test")
     shared_colnames = ["CHROM", "POS", "ID", "ALLELE_0", "ALLELE_1", "ALLELE_1_FREQ", "BETA", "SE", "LOG10P", "N"]
     regenie_only_colnames = ["TEST", "CHISQ", "EXTRA"]
     saige_only_colnames = ["ALLELE_1_COUNT", "MISSING_RATE", "T_STAT", "VAR"]
+    saige_binary_only_colnames = ["N_CASES", "N_CONTROLS", "PVAL_NA", "IS_SPA", "AF_CASES", "AF_CONTROLS", "N_CASES_HOM", "N_CASES_HET", "N_CONTROLS_HOM", "N_CONTROLS_HET"]
+
     tmpdir = mktempdir()
     # Test REGENIE
     gwas_results_file = joinpath(TESTDIR, "assets", "gwas_software_outputs", "all.AGE.chr1_AGE.regenie")
@@ -30,7 +32,7 @@ TESTDIR = joinpath(PKGDIR, "test")
     ## Expected colnames
     @test names(regenie_harmonized) == vcat(shared_colnames, regenie_only_colnames)
 
-    # Test SAIGE
+    # Test SAIGE continuous trait
     gwas_results_file = joinpath(TESTDIR, "assets", "gwas_software_outputs", "all.AGE.chr1.saige.txt")
     output_file = joinpath(tmpdir, "saige.harmonized.tsv")
     copy!(ARGS, [
@@ -46,6 +48,49 @@ TESTDIR = joinpath(PKGDIR, "test")
     @test size(saige_source) == size(saige_harmonized)
     ## Expected colnames
     @test names(saige_harmonized) == vcat(shared_colnames, saige_only_colnames)
+
+    # Test SAIGE binary trait
+    gwas_results_file = joinpath(tmpdir, "all.BINARY_TRAIT.chr1.saige.txt")
+    saige_output = Dict(
+        "CHR" => [1], 
+        "POS" => [2], 
+        "MarkerID" => ["rs134"], 
+        "Allele1" => ["A"], 
+        "Allele2" => ["C"], 
+        "AC_Allele2" => [1], 
+        "AF_Allele2" => [1], 
+        "MissingRate" => [0.1], 
+        "BETA" => [1], 
+        "SE" => [1], 
+        "Tstat" => [1], 
+        "var" => [1], 
+        "p.value" => [0.1], 
+        "p.value.NA" => [0.1], 
+        "Is.SPA" => [false], 
+        "AF_case" => [0.1], 
+        "AF_ctrl" => [0.1], 
+        "N_case" => [1], 
+        "N_ctrl" => [2], 
+        "N_case_hom" => [1], 
+        "N_case_het" => [1], 
+        "N_ctrl_hom" => [1], 
+        "N_ctrl_het" => [1]
+    )
+    CSV.write(gwas_results_file, saige_output, missingstring="NA")
+    output_file = joinpath(tmpdir, "saige.harmonized.tsv")
+    copy!(ARGS, [
+        "harmonize-gwas-results",
+        gwas_results_file,
+        "--source-software=saige",
+        "--output=$output_file"
+    ])
+    julia_main()
+    saige_source = CSV.read(gwas_results_file, DataFrame)
+    saige_harmonized = CSV.read(output_file, DataFrame)
+    ## Nothing is lost
+    @test size(saige_harmonized) == (1, length(saige_output) + 1) # N column added
+    ## Expected colnames
+    @test names(saige_harmonized) == vcat(shared_colnames, saige_only_colnames, saige_binary_only_colnames)
 end
 
 end
